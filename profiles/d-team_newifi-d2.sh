505d42c852bd466a0c2265ea1cb1dd5376f3534b
金哥 <zhoujinshi@qq.com>
2026-02-28 21:28:57 +0800

    update

--- profiles/d-team_newifi-d2.sh
+++ profiles/d-team_newifi-d2.sh
@@ -5,6 +5,8 @@
 # Lisence: MIT
 # Author: cnbbx
 #============================================================
+cp -f banner package/base-files/files/etc/banner
+
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
